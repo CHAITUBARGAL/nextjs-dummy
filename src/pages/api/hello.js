d7cb5d1a6f9cac4d587477b2cc7ce16627aa25dd
Chetan Bargal <chetan.bargal11@gmail.com>
2023-09-19 18:35:27 +0530

    completed

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,15 @@
+import { NextResponse } from "next/server";
+import {user} from "@/util/db";
+
+// export function GET(){
+//   const data= user;
+//   return NextResponse.json(data,{status:200})
+// }
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  // res.status(200).json({ name: 'John Doe' })
+  const data= user;
+  return NextResponse.json(data,{status:200})
 }
---
+++ src/util/db.js
@@ -0,0 +1,23 @@
+export const user = [
+    {
+        name:"anil",
+        age:12,
+        email:"anil@gmail.com"
+    },
+    {
+        name:"chetan",
+        age:13,
+        email:"chetan@gmail.com"
+    },
+    {
+        name:"tushar",
+        age:15,
+        email:"tushar@gmail.com"
+    },
+    {
+        name:"harshal",
+        age:10,
+        email:"harshal@gmail.com"
+    },
+
+]
